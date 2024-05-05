e3920a7d8c302564b83cfc05b1ddf7502e26d4f2
IzanagLP <143728588+IzanagLP@users.noreply.github.com>
2024-05-05 19:32:22 -0400

    Update Payments.py

--- Payments.py
+++ Payments.py
@@ -1,26 +1,34 @@
 import calendar
 
-#Takes the month and print 
-march = calendar.TextCalendar(calendar.MONDAY)
-days_month = []
-for day_number in march.itermonthdays2(2024, 5):
-    days_month.append(day_number)
+def main():
+    def payments(year, month):
+        calendar_class = calendar.TextCalendar(calendar.MONDAY)
+        
+        days_month = []
+        for day_number in calendar_class.itermonthdays2(year, month):
+            days_month.append(day_number)
 
-days_names = []
-for name in calendar.day_name:
-    days_names.append(name)
+        days_names = []
+        for name in calendar.day_name:
+            days_names.append(name)
 
-list_day_number_plus_day_name = []
-for day, day_name in days_month:
-    if day != 0:
-        list_day_number_plus_day_name.append([day, days_names[day_name]])
+        list_day_number_plus_day_name = []
+        for day, day_name in days_month:
+            if day != 0:
+                list_day_number_plus_day_name.append([day, days_names[day_name]])
 
-for day_number in list_day_number_plus_day_name:
-    print(day_number)
+        days_worked_counter = 0
+        for day_number, day_name in list_day_number_plus_day_name:
+            if day_name != "Tuesday":
+                days_worked_counter += 1
+        
+        salary = days_worked_counter * 100
 
-days_worked_counter = 0
-for day_number, day_name in list_day_number_plus_day_name:
-    if day_name != "Tuesday":
-        days_worked_counter += 1
+        return salary
 
-print(days_worked_counter)
+    year = 2024
+    month = int(input("Input the month number: "))
+    print(payments(year, month))
+
+if __name__ == "__main__":
+   main()
