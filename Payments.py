1f3bfda508b89d55a768cc544dcbce9002d713b2
IzanagLP <143728588+IzanagLP@users.noreply.github.com>
2024-05-05 20:22:25 -0400

    Update Payments.py

--- Payments.py
+++ Payments.py
@@ -17,18 +17,22 @@ def main():
             if day != 0:
                 list_day_number_plus_day_name.append([day, days_names[day_name]])
 
-        days_worked_counter = 0
+        days_worked = 0
         for day_number, day_name in list_day_number_plus_day_name:
             if day_name != "Tuesday":
-                days_worked_counter += 1
+                days_worked += 1
 
-        salary = days_worked_counter * 100
+        salary = days_worked * 100
 
         return salary
 
+    month_names = list(calendar.month_name)
+    month_numbers = list(range(13))
+    numered_months_list = list(zip(month_numbers, month_names))
+
     year = 2024
     month = int(input("Input the month number: "))
-    print(payments(year, month))
-
+    payment = payments(year, month)
+    print(f'On the month of {numered_months_list[month][1]} you will receive R${payment}.')
 if __name__ == "__main__":
    main()
