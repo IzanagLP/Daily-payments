9859c6dac4512587ed3809d14683d1f63a69d145
IzanagLP <143728588+IzanagLP@users.noreply.github.com>
2024-05-05 21:00:34 -0400

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+#Initialize app
+root = tk.Tk()
+root.title("Monthly Payment")
+root.eval("tk::PlaceWindow . center")
+
+frame1 = tk.Frame(root, width=500, height=600, bg="3d6466")
+
+
+
+#Run app
+root.mainloop()
